054ec80461f2ee8602949528c852ce4d11e5fe6c
Hiroyuki <dokomademo1124@gmail.com>
2021-07-11 18:20:08 +0900

    baseCode

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: false,// eslint-disable-line
+  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  parserOptions: {
+    parser: "babel-eslint",// eslint-disable-line
+
+  },
+  rules: {
+  },
+};
