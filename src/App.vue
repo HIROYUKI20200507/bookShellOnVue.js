2b5a13bc8c76e675a941d8306b0a020b44d4c855
Hiroyuki <dokomademo1124@gmail.com>
2021-08-03 09:51:03 +0900

    [modi]eslint削除+

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
     <v-app>
         <Header />
         <v-main>
-            <router-view />
+            <v-container>
+                <router-view
+                @add-book-list="addBook"/>
+            </v-container>
         </v-main>
         <Footer />
     </v-app>
@@ -11,17 +14,54 @@
 <script>
 import Header from '@/global/Header.vue'
 import Footer from '@/global/Footer.vue'
+const STORAGE_KEY = 'books'
 
 export default {
     name: "App",
+
     components: {
         Header,
         Footer
     },
+
     data() {
         return{
-
+            books: [],
+            newBook: null
         }
     },
+
+    mounted() {
+        if(localStorage.getItem(STORAGE_KEY)) {
+            try {
+                this.books = JSON.parse(localStorage.getItem(STORAGE_KEY));
+            } catch(e) {
+                localStorage.removeItem(STORAGE_KEY);
+            }
+        }
+    },
+
+    methods: {
+        addBook(e) {
+            this.books.push({
+                id: this.books.length,
+                title: e.title,
+                image: e.image,
+                description: e.description,
+                readDate: '',
+                memo: '',
+            });
+            // this.newBook = '';
+            this.saveBooks();
+        },
+        removeBook(x) {
+            this.books.splice(x, 1);
+            this.saveBooks();
+        },
+        saveBooks() {
+            const parsed = JSON.stringify(this.books);
+            localStorage.setItem(STORAGE_KEY, parsed);
+        }
+    }
 };
 </script>
